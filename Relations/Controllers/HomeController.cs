a0f93614e9884e0ce8f58e0a6832c178222f8566
Francois Vongue <francoisvongue@gmail.com>
2019-12-27 09:44:34 +0200

    added relationapp directory

---
+++ Relations/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RelationApp.Domain.Iterfaces;
+using RelationApp.Domain.Models;
+
+namespace RelationApp.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRelationService service;
+        public HomeController(IRelationService s)
+        {
+            service = s;
+        }
+
+        public IActionResult Index() => View(service.GetAll());
+        
+        [Route("Home/Index/sortfield")]
+        public IActionResult Index(string sortfield)
+        {
+            return View(service.GetOrdered(sortfield));
+        }
+
+        [HttpGet]
+        public IActionResult AddRelation() => View();
+
+        [HttpPost]
+        public IActionResult AddRelation(Relation relation)
+        {
+            service.Add(relation);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Relations/Startup.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using RelationApp.Persistence.Contexts;
+using RelationApp.Domain.Iterfaces;
+using RelationApp.Domain.Models;
+using RelationApp.Persistence.Repositories;
+using RelationApp.Services;
+
+namespace RelationApp.Client
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddSingleton<DbContext,RelationAppContext>();
+            services.AddSingleton<IRepository<Relation>, RelationRepository>();
+            services.AddSingleton<IRelationService, RelationService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{sortfield?}");
+            });
+        }
+    }
+}
